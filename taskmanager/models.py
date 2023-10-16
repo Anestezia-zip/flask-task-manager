6dd17e3c8ad86e6ce2e7bd8135f10eb9cd5d5257
Anestezia-zip <>
2023-10-16 10:01:18 +0000

    Categories

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -4,7 +4,9 @@ class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
-    tasks = db.relations("Task", backref="category", cascade="all, delete", lazy=True)
+    tasks = db.relationship(
+        "Task", backref="category", cascade="all, delete", lazy=True
+    )
 
     def __repr__(self):
         # to represent itself in the form of string
@@ -18,7 +20,8 @@ class Task(db.Model):
     task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)  # or db.DateTime or db.Time
-    category_id = db.Column(db.Integer, db.ForeignKey("category.id"), ondelete="CASCADE", nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+
 
     def __repr__(self):
         # to represent itself in the form of string
